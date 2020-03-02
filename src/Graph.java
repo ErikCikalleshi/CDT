495f272c9c620f4a4c11d4537613f12eb6f83929
Th3RapidK1ller <stcikeri@bx.fallmerayer.it>
2020-03-02 16:22:39 +0100

    Creating Graph

--- src/Graph.java
+++ src/Graph.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
-class Output extends Application {
 
+
+public class Graph extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         NumberAxis xAxis = new NumberAxis();
@@ -12,12 +15,24 @@ class Output extends Application {
         xAxis.setLabel("X: ");
         yAxis.setLabel("Y: ");
         LineChart<Number,Number> lineChart = new LineChart<Number,Number>(xAxis,yAxis);
+        lineChart.setTitle("Näherungsverfahren");
+        XYChart.Series series = new XYChart.Series();
+        series.setName("LUL");
+        for (double i = 0; i < 100; i+=0.1) {
+            series.getData().add(new XYChart.Data(i,Math.pow(i,2)));
+        }
+        for (double i = 0; i > -100; i-=0.1) {
+            series.getData().add(new XYChart.Data(i,Math.pow(i,2)));
+        }
+        Scene scene  = new Scene(lineChart,800,600);
+        lineChart.getData().add(series);
+
+        stage.setScene(scene);
+        stage.show();
     }
-}
 
-
-public class Graph {
     public static void main(String[] args){
+        launch(args);
 
     }
 }
